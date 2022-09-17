d6b68f90d39dead39c78c3f945dbfdf0c2d30fbd
Arjun Rao <rao.rjun@gmail.com>
2022-09-17 18:11:09 -0400

    added claw to robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Robot.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.ftc16072;
 
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Claw;
 import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Gyro;
 import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.MecanumDrive;
 import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Mechanism;
@@ -12,10 +14,12 @@ import java.util.List;
 public class Robot {
     public MecanumDrive mecanumDrive=new MecanumDrive();
     public Gyro gyro = new Gyro();
+    public Claw claw = new Claw();
 
     List<Mechanism> mechanismList = Arrays.asList(
             mecanumDrive,
-            gyro
+            gyro,
+            claw
     );
     public void init (HardwareMap hwMap){
         for(Mechanism mechanism : mechanismList){

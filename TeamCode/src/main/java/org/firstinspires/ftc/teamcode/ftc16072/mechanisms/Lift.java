dbaabd231475b2b0acf014321f0d4d8ae3fa9f96
Alan Wong <alanwong9664@gmail.com>
2022-09-19 20:18:33 -0400

    removed goto indvidual levels, replace with goto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Lift.java
@@ -23,24 +23,62 @@ import java.util.List;
     Intake --> HighPosition
     GroundPosition --> Intake
  */
-public class Lift extends Mechanism{
+public class Lift extends Mechanism {
     private DcMotorEx liftMotor;
-    private double GROUNDPOSITION = 0.2;
-    private double INTAKEPOSITION = 0.1;
-    private double LOWPOSITION = 0.4;
-    private double MIDDLEPOSITION = 0.6;
-    private double HIGHPOSITION = 0.8;
-    private static int slidesMin = 0;
-    private static int slidesMax = -3000;
+    public static int GROUND_POSITION = 200;
+    public static int INTAKE_POSITION = 100;
+    public static int LOW_POSITION = 100;
+    public static int MIDDLE_POSITION = 600;
+    public static int HIGH_POSITION = 800;
+    public static int slidesMin = 0;
+    public static int slidesMax = -3000;
     //TODO: find the right values and make final
-    public State state = State.INTAKE;
+    public Level state = Level.INTAKE;
 
-    public enum State {
+    public enum Level {
         INTAKE,
         GROUND,
         LOW,
         MIDDLE,
-        HIGH
+        HIGH;
+
+        public Level next(Level current) {
+            Level returnValue = current;
+            switch (current) {
+                case INTAKE:
+                    returnValue = GROUND;
+                    break;
+                case GROUND:
+                    returnValue = LOW;
+                    break;
+                case LOW:
+                    returnValue = MIDDLE;
+                    break;
+                case MIDDLE:
+                    returnValue = HIGH;
+                    break;
+            }
+            return returnValue;
+        }
+
+        public Level previous(Level current) {
+            Level returnValue = current;
+            switch (current) {
+                case HIGH:
+                    returnValue = MIDDLE;
+                    break;
+                case MIDDLE:
+                    returnValue = LOW;
+                    break;
+                case LOW:
+                    returnValue = GROUND;
+                    break;
+                case GROUND:
+                    returnValue = INTAKE;
+                    break;
+            }
+            return returnValue;
+        }
     }
 
     @Override
@@ -57,60 +95,54 @@ public class Lift extends Mechanism{
         );
 
     }
-    public void stopMotor(){
+
+    public void stopMotor() {
         liftMotor.setPower(0);
     }
-    public double getLiftPosition(){
+
+    public double getLiftPosition() {
         return liftMotor.getCurrentPosition();
     }
 
-    public void goToIntake(double power){
-        state = State.INTAKE;
-        retract(power);
-    }
-    public void goToGround(double power){
-        state = State.GROUND;
-        goToPosition(GROUNDPOSITION, power);
-    }
-    public void goToLow(double power){
-        state = State.LOW;
-        goToPosition(LOWPOSITION, power);
-    }
-    public void goToMiddle(double power){
-        state = State.MIDDLE;
-        goToPosition(MIDDLEPOSITION, power);
-    }
-    public void goToHigh(double power){
-        state = State.HIGH;
-        extend(power);
-    }
-    public void goToPosition(double position,double power){
-        if(liftMotor.getCurrentPosition()>position){
-            retract(power);
+    public void goTo(Level level) {
+        switch (level) {
+            case INTAKE:
+                liftMotor.setTargetPosition(INTAKE_POSITION);
+                break;
+            case GROUND:
+                liftMotor.setTargetPosition(GROUND_POSITION);
+                break;
+            case LOW:
+                liftMotor.setTargetPosition(LOW_POSITION);
+                break;
+            case MIDDLE:
+                liftMotor.setTargetPosition(MIDDLE_POSITION);
+                break;
+            case HIGH:
+                liftMotor.setTargetPosition(HIGH_POSITION);
+                break;
         }
-        else if(liftMotor.getCurrentPosition()<position){
-            extend(power);
-        }
-        else{
-            stopMotor();
-        }
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
-    public void extend(double power){
-        if(canExtend()){
-            liftMotor.setPower(power);
-        }
-    }
-    public void retract(double power){
-        if(canRetract()){
+
+
+    public void extend(double power) {
+        if (canExtend()) {
             liftMotor.setPower(power);
         }
     }
 
-    public boolean canExtend(){
+    public void retract(double power) {
+        if (canRetract()) {
+            liftMotor.setPower(power);
+        }
+    }
+
+    public boolean canExtend() {
         return liftMotor.getCurrentPosition() < slidesMin;
     }
 
-    public boolean canRetract(){
+    public boolean canRetract() {
         return liftMotor.getCurrentPosition() >= slidesMax;
     }
 

637ebac4ff638786d2358fabf5afbaac77642df7
Arjun Rao <rao.rjun@gmail.com>
2022-09-22 21:00:48 -0400

    matched the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/ParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/ParkAuto.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.ftc16072.Robot;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveBackwardAction;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveForwardAction;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveRightAction;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.QQAction;
 import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
@@ -13,10 +15,25 @@ import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
 public class ParkAuto extends QQOpMode {
     NavigationMecanum nav = new NavigationMecanum(robot);
     QQAction currentAction;
+    int parkingZone = 1;
     @Override
     public void init(){
         super.init();
-        currentAction = new DriveRightAction(36, DistanceUnit.INCH);
+
+    }
+    @Override
+    public void start(){
+        if(parkingZone==1){
+            currentAction = new DriveForwardAction(24,DistanceUnit.INCH);
+            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH));
+        }
+        else if(parkingZone==2){
+            currentAction = new DriveRightAction(36, DistanceUnit.INCH);
+        }
+        else{
+            currentAction = new DriveBackwardAction(24,DistanceUnit.INCH);
+            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH));
+        }
     }
 
 

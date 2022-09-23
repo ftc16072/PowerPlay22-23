16548edf0292245c1cee322b69d9e849ed1ad862
Arjun Rao <rao.rjun@gmail.com>
2022-09-23 18:53:01 -0400

    mirrored auto for the other side, added a drive left action and created a separate red auto file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/BlueParkAuto.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.ftc16072.Robot;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveBackwardAction;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveForwardAction;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveRightAction;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.QQAction;
+import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
+
+@Autonomous
+public class BlueParkAuto extends QQOpMode {
+    //NavigationMecanum nav = new NavigationMecanum(robot);
+    QQAction currentAction;
+    int parkingZone = 1;
+    @Override
+    public void init(){
+        super.init();
+
+    }
+    @Override
+    public void start(){
+        if(parkingZone==1){
+            currentAction = new DriveForwardAction(24,DistanceUnit.INCH);
+            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH)); // goes to 1st parking zone
+        }
+        else if(parkingZone==2){
+            currentAction = new DriveRightAction(36, DistanceUnit.INCH); // goes to 2nd parking zone
+        }
+        else{
+            currentAction = new DriveBackwardAction(24,DistanceUnit.INCH);
+            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH)); // goes to 3rd parking zone
+        }
+    }
+
+
+    @Override
+    public void loop() {
+        if(currentAction!=null){
+            currentAction = currentAction.run(this);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/RedParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/RedParkAuto.java
@@ -1,44 +1,44 @@
 package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.ftc16072.Robot;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveBackwardAction;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveForwardAction;
+import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveLeftAction;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.DriveRightAction;
 import org.firstinspires.ftc.teamcode.ftc16072.actions.QQAction;
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.MecanumDrive;
 import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
 
 @Autonomous
-public class ParkAuto extends QQOpMode {
+public class RedParkAuto extends QQOpMode{
     NavigationMecanum nav = new NavigationMecanum(robot);
     QQAction currentAction;
     int parkingZone = 1;
-    @Override
-    public void init(){
-        super.init();
 
+    @Override
+    public void init() {
+        super.init();
     }
+
     @Override
     public void start(){
         if(parkingZone==1){
-            currentAction = new DriveForwardAction(24,DistanceUnit.INCH);
-            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH)); // goes to 1st parking zone
+            currentAction = new DriveForwardAction(24, DistanceUnit.INCH);
+            currentAction.setLast(new DriveLeftAction(36, DistanceUnit.INCH)); // goes to 1st parking zone
         }
-        else if(parkingZone==2){
-            currentAction = new DriveRightAction(36, DistanceUnit.INCH); // goes to 2nd parking zone
+        if(parkingZone==2){
+            currentAction = new DriveLeftAction(36, DistanceUnit.INCH); //goes to 2nd parking zone
         }
-        else{
+        if(parkingZone==3){
             currentAction = new DriveBackwardAction(24,DistanceUnit.INCH);
-            currentAction.setLast(new DriveRightAction(36, DistanceUnit.INCH)); // goes to 3rd parking zone
+            currentAction.setLast(new DriveLeftAction(36, DistanceUnit.INCH)); // goes to 3rd parking zone
         }
     }
 
-
     @Override
-    public void loop() {
+    public void loop(){
         if(currentAction!=null){
             currentAction = currentAction.run(this);
         }

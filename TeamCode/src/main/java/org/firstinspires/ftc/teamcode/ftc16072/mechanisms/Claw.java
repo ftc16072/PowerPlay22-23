51c6f4995c4244ec8f5aae61503c50a31929ea68
Joshua-Smith-42 <joshua@randomsmiths.com>
2022-09-24 13:37:26 -0400

    renamed positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Claw.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,11 +12,12 @@ import org.firstinspires.ftc.teamcode.ftc16072.tests.TestServo;
 import java.util.Arrays;
 import java.util.List;
 
+@Config
 public class Claw extends Mechanism{
     private Servo clawServo;
     private ColorRangeSensor coneDetector;
-    private static double GRIPPED = 0.5;
-    private static double RELEASED = 0.0;
+    public static double GRIPPED_SERVO_POSITION = 0.5;
+    public static double RELEASED_SERVO_POSITION = 0.0;
 //make final when values fixed
     public enum State{
         EMPTY,
@@ -49,16 +51,16 @@ public class Claw extends Mechanism{
     @Override
     public List<QQTest> getTests() {
         return Arrays.asList(
-                new TestServo(clawServo, "claw", 0.2, 0),
+                new TestServo(clawServo, "claw", GRIPPED_SERVO_POSITION, RELEASED_SERVO_POSITION),
                 new TestColorSensor(coneDetector, "cone_detector")
         );
     }
     public void grip(){
-        clawServo.setPosition(GRIPPED);
+        clawServo.setPosition(GRIPPED_SERVO_POSITION);
         state = State.GRIPPED;
     }
     public void release(){
-        clawServo.setPosition(RELEASED);
+        clawServo.setPosition(RELEASED_SERVO_POSITION);
     }
     public double getClawPosition(){
         return clawServo.getPosition();

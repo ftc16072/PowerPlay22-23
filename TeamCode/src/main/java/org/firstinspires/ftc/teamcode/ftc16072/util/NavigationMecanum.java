b3b8b1fda22eb874b35798049e6739254be1542d
Alan Wong <alanwong9664@gmail.com>
2022-09-24 17:43:10 -0400

    control scheme - snap turns implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/util/NavigationMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/util/NavigationMecanum.java
@@ -6,12 +6,13 @@ import org.firstinspires.ftc.teamcode.ftc16072.Robot;
 public class NavigationMecanum {
     public Robot robot;
     public double TURN_TOLERANCE = 5.0;
+    public double desiredHeading;
 
-    public NavigationMecanum(Robot robot){
+    public NavigationMecanum(Robot robot) {
         this.robot = robot;
     }
 
-    public void driveFieldRelative(double forward, double right, double rotateSpeed){
+    public void driveFieldRelative(double forward, double right, double rotateSpeed) {
         double heading = robot.gyro.getHeading(AngleUnit.RADIANS);
         Polar drive = new Polar(right, forward);
         drive.rotate(-heading, AngleUnit.RADIANS);
@@ -20,12 +21,31 @@ public class NavigationMecanum {
     }
 
 
-    public boolean snapTurnCW(){
+    public boolean snapTurnCCW(boolean isTurning) {
         double heading = robot.gyro.getHeading(AngleUnit.DEGREES);
-        return false;
+        if (!isTurning) {
+            if (heading > 0 && heading <= 90) {
+                desiredHeading = 0;
+            } else if (heading > 90 && heading <= 180) {
+                desiredHeading = 90;
+            } else if (heading > 180 && heading <= 270) {
+                desiredHeading = 180;
+            } else if (heading > 270 && heading <= 360 || heading == 0) {
+                desiredHeading = 270;
+            }
+        }
+        double diffAngle = Math.abs(heading-desiredHeading);
+        if(diffAngle<=TURN_TOLERANCE){
+            robot.mecanumDrive.drive(0,0,-0.5);
+            return false;
+        }
+        else{
+            robot.mecanumDrive.drive(0,0,0);
+        }
+        return true;
     }
 
-    public boolean snapTurnCCW(){
+    public boolean snapTurnCW(boolean isTurning) {
         return false;
     }
 }

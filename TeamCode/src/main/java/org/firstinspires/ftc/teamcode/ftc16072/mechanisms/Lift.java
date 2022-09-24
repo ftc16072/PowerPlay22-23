8cd3f684882c372eac4b744f600fb588dd2c1bdc
Joshua Smith <joshua@randomsmiths.com>
2022-09-24 15:26:10 -0400

    Lift now working (jerky on way down)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Lift.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.ftc16072.mechanisms;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.ftc16072.tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.tests.TestMotor;
@@ -23,16 +25,23 @@ import java.util.List;
     Intake --> HighPosition
     GroundPosition --> Intake
  */
+@Config
 public class Lift extends Mechanism {
-    private DcMotorEx liftMotor;
     public static int GROUND_POSITION = 200;
-    public static int INTAKE_POSITION = 100;
+    public static int INTAKE_POSITION = 80;
     public static int LOW_POSITION = 100;
     public static int MIDDLE_POSITION = 600;
-    public static int HIGH_POSITION = 800;
+    public static int HIGH_POSITION = 2900;
     public static int slidesMin = 0;
-    public static int slidesMax = -3000;
+    public static int slidesMax = 3000;
+    public static double P;
+    public static double I;
+    public static double D;
+    public static double F;
+    private PIDFCoefficients liftMotorPIDF;
+
     //TODO: find the right values and make final
+    public DcMotorEx liftMotor;
     public Level state = Level.INTAKE;
 
     public enum Level {
@@ -86,13 +95,18 @@ public class Lift extends Mechanism {
         liftMotor = hwMap.get(DcMotorEx.class, "lift_motor");
         liftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftMotorPIDF = liftMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION);
+        P = liftMotorPIDF.p;
+        D = liftMotorPIDF.d;
+        I = liftMotorPIDF.i;
+        F = liftMotorPIDF.f;
     }
 
     @Override
     public List<QQTest> getTests() {
         return Arrays.asList(
-                new TestMotor(liftMotor, "lift_motor_up", -0.3),
-                new TestMotor(liftMotor, "lift_motor down", 0.3)
+                new TestMotor(liftMotor, "lift_motor_up", 0.3),
+                new TestMotor(liftMotor, "lift_motor down", -0.3)
         );
 
     }
@@ -106,6 +120,11 @@ public class Lift extends Mechanism {
     }
 
     public void goTo(Level level) {
+        liftMotorPIDF.p = P;
+        liftMotorPIDF.i = I;
+        liftMotorPIDF.d = D;
+        liftMotorPIDF.f = F;
+        liftMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION, liftMotorPIDF);
         switch (level) {
             case INTAKE:
                 liftMotor.setTargetPosition(INTAKE_POSITION);
@@ -122,8 +141,11 @@ public class Lift extends Mechanism {
             case HIGH:
                 liftMotor.setTargetPosition(HIGH_POSITION);
                 break;
+            default:
+                liftMotor.setTargetPosition(0);
         }
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(0.5);
     }
 
 

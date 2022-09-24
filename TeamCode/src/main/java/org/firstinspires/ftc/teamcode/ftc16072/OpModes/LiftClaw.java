762acc1fb9a146c3864ca9cadbadebaa4ede6e9a
Joshua Smith <joshua@randomsmiths.com>
2022-09-24 15:26:10 -0400

    Lift now working (jerky on way down)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/LiftClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/LiftClaw.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Lift;
 import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
 
 @TeleOp()
 public class LiftClaw extends QQOpMode {
+    boolean wasDown;
+    boolean wasUp;
 
     @Override
     public void loop() {
@@ -14,6 +18,19 @@ public class LiftClaw extends QQOpMode {
         }else if (gamepad1.b){
             robot.claw.release();
         }
+        if (gamepad1.dpad_down && !wasDown){
+            robot.lift.goTo(Lift.Level.INTAKE);
+            telemetry.addData("Going to", "Intake");
+        } else if (gamepad1.dpad_up && !wasUp){
+            robot.lift.goTo(Lift.Level.HIGH);
+            telemetry.addData("Going to", "High");
+        }
+        wasDown = gamepad1.dpad_down;
+        wasUp = gamepad1.dpad_up;
+
+        telemetry.addData("Lift motor position", robot.lift.liftMotor.getCurrentPosition());
+        telemetry.addData("Lift motor target", robot.lift.liftMotor.getTargetPosition());
+        telemetry.addData("Lift PIDF", robot.lift.liftMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION));
 
 
     }

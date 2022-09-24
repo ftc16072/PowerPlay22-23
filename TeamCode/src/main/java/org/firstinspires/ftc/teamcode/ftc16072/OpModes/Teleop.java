6d2437f0613c6150bb0bfb747f94b9bf3919a432
Alan Wong <alanwong9664@gmail.com>
2022-09-24 17:05:05 -0400

    initial control scheme

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/Teleop.java
@@ -4,100 +4,70 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ftc16072.Robot;
+import org.firstinspires.ftc.teamcode.ftc16072.mechanisms.Lift;
 import org.firstinspires.ftc.teamcode.ftc16072.util.NavigationMecanum;
 
 @TeleOp()
-public class Teleop extends OpMode {
+public class Teleop extends QQOpMode {
     Robot robot = new Robot();
     NavigationMecanum nav = new NavigationMecanum(robot);
-   // int[] liftLevels = {0,1,2,3,4};
-    int liftLevel = 0;
-    boolean clawOpen = false;
-    boolean wasUp, wasDown;
-    boolean LSD, RSD = false; //LSD=Left stick down, RSD = right stick down
-    boolean isLeft = true;
 
 
-    @Override
-    public void init() {
-        robot.init(hardwareMap);
-    }
+// Control scheme
+// left stick = strafing
+// right stick = turning
 
-    private void overrideLoop(){
-        //manual override for lift
-        if(gamepad1.dpad_up){
-            robot.lift.extend(0.5);
-        }else{
-            robot.lift.stopMotor();
-        }
 
-        if(gamepad1.dpad_down){
-            robot.lift.retract(0.5);
-        }else{
-            robot.lift.stopMotor();
-        }
-    }
-    private void normalLoop(){
-        //lift up and down - snapping on levels
-        if (gamepad1.dpad_down && !wasDown) {
-            //go to previous
-        }
-        wasDown = gamepad1.dpad_down;
+// a = intake position(lift)
+// x = low position(lift)
+// y = medium position(lift)
+// b = high position(lift)
+// d pad up = manual lift up
+// d pad down = manual lift down
+// d pad right = stop manual lift
+// right bumper = grip (claw)
+// left bumper = release (claw)
 
-        if (gamepad1.dpad_up && !wasUp) {
-            //go to next
-        }
-        wasUp = gamepad1.dpad_up;
-
-    }
-
+// right trigger = turn 90 degrees robot CW
+// left trigger = turn 90 degrees robot CCW
     @Override
     public void loop() {
-        if(gamepad1.b){
-            overrideLoop();
-        }
-        else{
-            normalLoop();
-        }
-
-
-
-
-        //claw
-        if(gamepad1.right_bumper){
-            if(clawOpen == false){
-                robot.claw.grip();
-                clawOpen = true;
-            }else{
-                robot.claw.release();
-                clawOpen = false;
-            }
-        }
-
-        //lift shortcuts
-        if(gamepad1.y){
-
-        }
-
-        //snap turns
-        if(gamepad1.left_stick_button){ //&& !LSD){
-            nav.snapTurns(-gamepad1.left_stick_y, gamepad1.left_stick_x,true);
-        }
-        //LSD = gamepad1.left_stick_button;
-        if(gamepad1.right_stick_button){ //&& !RSD){
-            nav.snapTurns(-gamepad1.left_stick_y, gamepad1.left_stick_x,false);
-        }
-        //RSD = gamepad1.right_stick_button;
-
         //driver controls
         nav.driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        if (gamepad1.a) {
-            robot.mecanumDrive.setEncodeOffsets();
+        if (gamepad1.a){
+            robot.lift.goTo(Lift.Level.INTAKE);
+        }
+        else if (gamepad1.x){
+            robot.lift.goTo(Lift.Level.LOW);
+        }
+        else if (gamepad1.y){
+            robot.lift.goTo(Lift.Level.MIDDLE);
+        }
+        else if (gamepad1.b){
+            robot.lift.goTo(Lift.Level.HIGH);
+        }
+        else if (gamepad1.dpad_up){
+            robot.lift.extend(0.5);
+        }
+        else if (gamepad1.dpad_down){
+            robot.lift.retract(0.5);
+        }
+        else if (gamepad1.dpad_right){
+            robot.lift.stopMotor();
         }
 
-        double[] distances = robot.mecanumDrive.getDistance();
-        telemetry.addData("Distance driven forward", distances[0]);
-        telemetry.addData("distance strafed", distances[1]);
+        if (gamepad1.right_bumper){
+            robot.claw.grip();
+        }
+        else if (gamepad1.left_bumper){
+            robot.claw.release();
+        }
+        if (gamepad1.right_trigger > 0.5){
+            nav.snapTurnCW();
+        }
+        else if (gamepad1.left_trigger > 0.5){
+            nav.snapTurnCCW();
+        }
 
     }
 }

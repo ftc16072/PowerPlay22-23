c2217cf2aa2c53078750097402a4623437cffe39
preetithirukonda <preetithirukonda@gmail.com>
2022-10-10 21:29:42 -0400

    coreinrange and corebitwise semi done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/pipelines/SignalSleevePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/pipelines/SignalSleevePipeline.java
@@ -10,6 +10,15 @@ import org.openftc.easyopencv.OpenCvPipeline;
 public class SignalSleevePipeline extends OpenCvPipeline {
     //creates a rectangle to look in
     public Rect rect1 = new Rect(175, 120, 10, 10);
+
+    /*int i = 0;
+    for (y = 0, y <= 4,y++ ){
+
+        for (x = 0, y <= 4, x++) {
+            rectArray[i] = new Rect(115+x*20,75+y*20,10,10);
+            i++;
+        }
+    }  */
     //creates the color for the rectangle
     public Scalar rectangleColor = new Scalar(255, 255);
     //hue saturation brightness
@@ -20,11 +29,15 @@ public class SignalSleevePipeline extends OpenCvPipeline {
 
     @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2BGR);
+        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
         Imgproc.rectangle(input, rect1, rectangleColor);
         Mat submat = input.submat(rect1);
-        numberOfDots = getNumberOfDots(Core.mean(submat));
-        values = getValues((Core.mean(submat)));
+        Mat outmat = new Mat();
+        Mat outmat1 = new Mat();
+        Core.inRange(submat, (), (),outmat)
+
+        Core.bitwise_and(submat, submat,outmat1,outmat)
+
         return input;
     }
 
@@ -32,20 +45,13 @@ public class SignalSleevePipeline extends OpenCvPipeline {
         return "" + color.val[0] + " " + color.val[1] + " " + color.val[2];
     }
 
-    private int getNumberOfDots(Scalar color) {
-        double saturation = color.val[1];
-        //identfies number of dots based on color
-        if (saturation > 125) {
-            //green
-            return 3;
-
+    private int getNumberOfDots(Scalar[] color) {
+        int dots = 0;
+        for(Scalar testCol : color) {
+            double hue = testCol.val[0];
+            //put conditional in here
         }
-        if (saturation < 75) {
-            //black
-            return 1;
-        }
-        //red
-        return 2;
+        return dots;
 
     }
 }

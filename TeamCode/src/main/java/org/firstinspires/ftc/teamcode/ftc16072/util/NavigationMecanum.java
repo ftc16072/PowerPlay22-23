de9e9c171084b63cd0afddcfd824e4327bfb8c9f
Joshua <joshua@randomsmiths.com>
2023-01-21 09:45:25 -0500

    comp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/util/NavigationMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/util/NavigationMecanum.java
@@ -146,7 +146,7 @@ public class NavigationMecanum {
 
     public void setCurrentPosition(RobotPose pose) {
         currentPosition = pose;
-        mecanumDrive.setEncodeOffsets();
+        robot.mecanumDrive.setEncodeOffsets();
     }
 
 

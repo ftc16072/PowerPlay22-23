fa09792bdc95e3b3facd3a3c2982982fadccf456
Alan Wong <alanwong9664@gmail.com>
2023-02-07 19:31:10 -0500

    added getdistance method to the odometry class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/mechanisms/Odometry.java
@@ -15,6 +15,8 @@ public class Odometry extends Mechanism {
     private DcMotor forwardEncoder;
     private DcMotor strafeEncoder;
 
+    public double oldStrafeEncoderValue;
+    public double oldForwardEncoderValue;
     public double TICKS_PER_ROTATION = 8125;
     private double ENCODER_WHEEL_DIAMETER_MM = 38;
     public double CM_PER_ROTATION = (ENCODER_WHEEL_DIAMETER_MM/10)*Math.PI;
@@ -30,6 +32,8 @@ public class Odometry extends Mechanism {
     public void init(HardwareMap hwMap) {
         forwardEncoder = hwMap.get(DcMotor.class,"forward_encoder");
         strafeEncoder  = hwMap.get(DcMotor.class, "strafe_encoder");
+        oldForwardEncoderValue = forwardEncoder.getCurrentPosition();
+        oldStrafeEncoderValue = strafeEncoder.getCurrentPosition();
     }
 
     @Override
@@ -38,7 +42,11 @@ public class Odometry extends Mechanism {
                 new TestMotor(forwardEncoder, "Forward Encoder", 0),
                 new TestMotor(strafeEncoder, "Strafe Encoder", 0));
     }
-    public MoveDeltas getDistance(boolean reset) {
+    public double[] getDistance(boolean reset) {
+        double[] distances = {0.0, 0.0};
+        distances[0] = forwardEncoder.getCurrentPosition()-oldForwardEncoderValue;
+        distances[1] = strafeEncoder.getCurrentPosition()-oldStrafeEncoderValue;
+        return distances;
     }
 
 }
